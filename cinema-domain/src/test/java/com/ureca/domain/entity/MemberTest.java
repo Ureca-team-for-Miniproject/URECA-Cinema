f76d0488d0b87120d06092a36c4f44a6d8d46595
heogeonho <geonh1016@gmail.com>
2024-09-10 10:56:51 +0900

    :sparkles: #3 Member, MembershipRank 엔티티 테스트 코드 구현

---
+++ cinema-domain/src/test/java/com/ureca/domain/entity/MemberTest.java
@@ -0,0 +1,59 @@
+package com.ureca.domain.entity;
+
+import com.ureca.domain.repository.MemberRepository;
+import com.ureca.domain.repository.MembershipRankRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    private MembershipRankRepository membershipRankRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void test() {
+        MembershipRank membershipRank = MembershipRank.builder()
+                .code(4)
+                .rank("VIP")
+                .acmltRate(0.1)
+                .build();
+
+        membershipRankRepository.save(membershipRank); // 먼저 저장
+
+        Member member = Member.builder()
+                .id("testUser123")
+                .password("password123")
+                .name("John Doe")
+                .phone("010-1234-5678")
+                .birth(new Date())
+                .totalReservedTickets(10)
+                .acmltCnt(5)
+                .code(membershipRank)
+                .build();
+
+        // When: Member 저장
+        memberRepository.save(member);
+
+
+        // Then: 저장한 엔티티를 조회하고 값이 올바른지 확인
+        Member foundMember = memberRepository.findById("testUser123").orElse(null);
+        assertNotNull(foundMember); // 조회한 값이 null이 아님을 확인
+        assertEquals("testUser123", foundMember.getId());
+        assertEquals("John Doe", foundMember.getName());
+
+        // 출력 확인
+        System.out.println("===================================");
+        System.out.println(foundMember);
+    }
+}
