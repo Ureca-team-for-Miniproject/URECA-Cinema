b6c3f5cfb83d145897c1ba2c4fe1ac1cbcae4923
seojeongwoo <qw4643987@naver.com>
2024-09-22 00:18:43 +0900

    :heavy_plus_sign: #12 로그인 관련 컨트롤러 추가

---
+++ cinema-api/src/main/java/com/ureca/api/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.ureca.api.controller;
+
+import com.ureca.api.dto.request.NonMemberRequest;
+import com.ureca.domain.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@RequestMapping("/cinema")
+@Controller
+@AllArgsConstructor
+public class AuthController {
+
+    private UserService loginService;
+
+    @GetMapping("/login/member")
+    public String memberLogin(
+            @RequestParam(value = "error", required = false) String error, Model model) {
+        model.addAttribute("error", error);
+        return "loginMember";
+    }
+
+    @GetMapping("/login/nonmember")
+    public String nonmemberLogin(Model model) {
+        model.addAttribute("nonMemberRequest", NonMemberRequest.builder().build());
+        return "loginNonMember";
+    }
+
+    @PostMapping("/login/nonmember")
+    public String nonmemberLogin(
+            @ModelAttribute @Valid NonMemberRequest nonMember,
+            RedirectAttributes redirectAttributes) {
+        try {
+            loginService.nonMemberLogin(nonMember.toDto());
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "오류가 발생했습니다. 다시 시도해주세요.");
+            return "redirect:cinema/login/nonmember";
+        }
+
+        // TODO: 향후 비회원 로그인 이후 이동할 페이지 설정
+        return "redirect:cinema/home";
+    }
+}
